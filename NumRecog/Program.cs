86943f6341f7262b73e5e06279a2a55c422fb918
unknown <zhac361@live.rhul.ac.uk>
2022-02-18 18:54:16 +0000

    Changed path to dataset to be rleative to 'Program.cs' path and removed unused system inport. -Will

--- NumRecog/Program.cs
+++ NumRecog/Program.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using NumRecog;
+﻿using NumRecog;
 
 namespace MyProgram
 {
     class Program
     {
-        static string dataBaseFilePath = "C:/Users/Will/Documents/Computer Science Year 1/MyProjects/C# ML/Hand written images/mnist_784.csv";
+        static string dataBaseFilePath = "../Hand written images/mnist_784.csv";
         static void Main(string[] args)
         {
             ImageSet images = new ImageSet(dataBaseFilePath);
 
             NeuralNet nn = new NeuralNet(images.getImages(), 60000);
-
-            //Console.WriteLine(nn.testCostRandomImage());
 
             while (true)
             {
