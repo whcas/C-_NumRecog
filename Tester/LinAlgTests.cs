f2a3143ec2496eaa70f76bd2825f5e4cf73180d8
unknown <zhac361@live.rhul.ac.uk>
2022-02-18 22:15:42 +0000

    Added test for 'vectorScaling'. -Will

--- Tester/LinAlgTests.cs
+++ Tester/LinAlgTests.cs
@@ -47,5 +47,21 @@ namespace Tester
 
             Assert.IsTrue(LinAlg.vectorsAreEqual(column, experimentalResult));
         }
+        
+        [TestMethod]
+        public void vectorScalingTest()
+        {
+            double[] v1 = {3, 8, 5};
+            double scaler = 9;
+            double[] result = {
+                3 * 9,
+                8 * 9,
+                5 * 9
+            };
+
+            double[] experimentalResult = LinAlg.vectorScaling(v1, scaler);
+
+            Assert.IsTrue(LinAlg.vectorsAreEqual(result, experimentalResult));
+        }
     }
 }
